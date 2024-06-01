655e83fb300f6ee3e60c8189f5dd3f70a1fba8d9
Azeem, Hafiz Muhammad <HafizMuhammad_Azeem@next.co.uk>
2024-06-01 19:57:09 +0100

    Day 1 & 2

---
+++ day-01/checkpoint_01.swift
@@ -0,0 +1,9 @@
+// Checkpoint-1
+
+let currentTemperatureInCelsius = 37.5
+
+let temperatureInFahrenheit = currentTemperatureInCelsius * 9 / 5 + 32
+
+print("Current Temperature in Celsius: \(currentTemperatureInCelsius)°")
+
+print("Temperature in Fahrenheit: \(temperatureInFahrenheit)")
