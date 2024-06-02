a75e9280a1d50c12a9a4da58d8d29d72ee4f1260
Azeem, Hafiz Muhammad <HafizMuhammad_Azeem@next.co.uk>
2024-06-02 13:50:27 +0100

    checkpoint 2 completed

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 100 Days of Swift
 
-Day 1: 
+Day 1 & 2: Checkpoint 1 code added
+
+Day 3: Checkpoint 2 code added
---
+++ checkpoint02/code.swift
@@ -0,0 +1,7 @@
+let colors : [String] = ["Red", "Blue", "Black", "Purple", "Orange", "Red"]
+
+print("Total Count: \(colors.count)")
+
+let colorsSet = Set(colors)
+
+print("Unique Count: \(colorsSet.count)")
